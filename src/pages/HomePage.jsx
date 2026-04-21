30e1d720bb40c23ebad6981714890b57cd91cc28
pietrobertino <pietrobertino20@gmail.com>
2026-04-21 12:58:59 +0200

    connect front end to back end

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
 export default function HomePage() {
+
+    const [movies, setMovies] = useState([]);
+
+    const url = import.meta.env.VITE_SERVER_API_URL + '/api/movies';
+
+    useEffect(() => {
+
+        axios.get(url)
+            .then(res => {
+                console.log(res.data)
+            })
+
+    }, []);
 
     return (
         <h1>Home page</h1>
