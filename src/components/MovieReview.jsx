45f9752dc243beefb9d2293570c443a5e4e097e8
pietrobertino <pietrobertino20@gmail.com>
2026-04-22 17:29:17 +0200

    add star visual to the reviews

--- src/components/MovieReview.jsx
+++ src/components/MovieReview.jsx
@@ -1,10 +1,31 @@
+import { useState, useEffect } from 'react';
+
 export default function MovieReview({ review }) {
+
+    const [stars, setStars] = useState([]);
+
+    useEffect(() => {
+        const array = [];
+        for (let i = 0; i < 5; i++) {
+            if (i < review.vote) {
+                array.push("star-fill")
+            } else {
+                array.push("star")
+            }
+        }
+        console.log(array)
+        setStars(array);
+    }, [])
 
     return (
         <li className=" list-group-item" key={review.id}>
             <div className="d-flex justify-content-between">
                 <div>{review.name}:</div>
-                <div>{review.vote}/5</div>
+                <div>
+                    {stars.map((star, index) => (
+                        <i className={`bi bi-${star}`} key={index}></i>
+                    ))}
+                </div>
             </div>
             <div className="review-text">
                 {review.text}
